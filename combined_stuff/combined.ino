90e3c523eceba08fe176b382cefbc80e8c9f2cc2
dorkboysupreme <166717444+dorkboysupreme@users.noreply.github.com>
2024-11-04 15:32:55 +0530

    Update combined.ino

--- combined_stuff/combined.ino
+++ combined_stuff/combined.ino
@@ -1,4 +1,29 @@
 #define sensorPin A0
+#define SOUND_SENSOR_PIN 7  // Sound sensor connected to pin 7
+#define RELAY_PIN 8        // Relay connected to pin 8
+#define NOTE_C4  262
+#define NOTE_G3  196
+#define NOTE_A3  220
+#define NOTE_B3  247
+#define NOTE_C4  262
+
+const int trigPin = 9;
+const int echoPin = 10;
+const int outputPin = 13;  //output pin
+const int motionThreshold = 5; 
+long previousDistance = 0;
+long currentDistance = 0;
+unsigned long lastEvent = 0;    // Time of last detected sound
+boolean relayState = false;     // Current state of relay
+const int buttonPin = 2; // Pin connected to the button
+const int buzzerPin = 8; // Pin connected to the buzzer
+
+int melody[] = { NOTE_G3, NOTE_B3 };
+int noteDurations[] = { 8, 8 }; // note durations: 4 = quarter note, 8 = eighth note, etc.
+bool running = false; // Tracks if melody is playing
+
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -49,45 +74,27 @@ int readSensor() {
   return outputValue;             //return the analog value we are getting from the sensor
 }
 
-#define SOUND_SENSOR_PIN 7  // Sound sensor connected to pin 7
-#define RELAY_PIN 8        // Relay connected to pin 8
-
-// Global variables
-unsigned long lastEvent = 0;    // Time of last detected sound
-boolean relayState = false;     // Current state of relay
-
 // Function to handle clap detection and relay control
 boolean handleClapDetection(int sensorPin, int relayPin, unsigned long debounceTime = 25) {
   // Read sound sensor
   int sensorData = digitalRead(sensorPin);
-  
   // Check if sound is detected (LOW indicates sound detection)
   if (sensorData == LOW) {
     // Check if enough time has passed since last detection
     if (millis() - lastEvent > debounceTime) {
       // Toggle relay state
       relayState = !relayState;
-      
       // Update relay output
       digitalWrite(relayPin, relayState);
-      
       // Debug output
       Serial.print("Clap detected! Relay state: ");
       Serial.println(relayState ? "ON" : "OFF");
     }
-    
     // Update last event time
     lastEvent = millis();
   }
-  
   return relayState;
 }
-const int trigPin = 9;
-const int echoPin = 10;
-const int outputPin = 13;  //output pin
-const int motionThreshold = 5; 
-long previousDistance = 0;
-long currentDistance = 0;
 
 
 long measureDistance() {
@@ -104,20 +111,6 @@ long measureDistance() {
 
   return distance;
 }
-#define NOTE_C4  262
-#define NOTE_G3  196
-#define NOTE_A3  220
-#define NOTE_B3  247
-#define NOTE_C4  262
-
-// Pin setup
-const int buttonPin = 2; // Pin connected to the button
-const int buzzerPin = 8; // Pin connected to the buzzer
-
-// Notes in the melody
-int melody[] = { NOTE_G3, NOTE_B3 };
-int noteDurations[] = { 8, 8 }; // note durations: 4 = quarter note, 8 = eighth note, etc.
-bool running = false; // Tracks if melody is playing
 
 void playMelody() {
   running = true; // Start playing melody
